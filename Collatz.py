1f8e63f97de73a807135dd68cad6238ac661f7d4
LYDIA <47765914+lydialoveworld@users.noreply.github.com>
2019-03-14 12:01:03 +0700

    Add files via upload

---
+++ Collatz.py
@@ -0,0 +1,12 @@
+def f(num):
+    if num%2==0:
+        return num//2 # 5/2 -- 2.5  5//2- 2
+    else:
+        return (num*3) +1
+
+
+
+print(f(f(f(f(f(f(f(674))))))))
+
+print(f(f(f(f(f(f(f(f(f(f(f(f(f(f(1071)))))))))))))))
+
